2adef88776130c6a80d61130c146fdf59d473970
Aditya Gangwal <adi@Adityas-MacBook-Pro.local>
2025-09-03 08:40:22 +0530

    Initial Commit

---
+++ src/main/java/com/problems/SpringBootApplication.java
@@ -0,0 +1,11 @@
+package com.problems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MachineCodingPracticeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MachineCodingPracticeApplication.class, args);
+    }
+}
